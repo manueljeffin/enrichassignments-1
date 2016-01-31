ddaf6d134f7c139c54b82fa6e911e37e1c892efb
VinithaCse <vinitharaocs@gmail.com>
2016-01-31 15:09:56 +0530

    Create sqb.c

---
+++ sqb.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  float a=0.000001,g,x=1;
+  scanf("%d",&n);
+  g = n;
+  while((g-x) > a)
+  {
+    g = (g+y)/2;
+    x = n/g;
+  }
+  printf("The square root is %f", x);
+}  
+  
