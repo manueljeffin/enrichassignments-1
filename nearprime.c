cab0b471f772a24971671fa08c8e55ab7b6fe860
VinithaCse <vinitharaocs@gmail.com>
2016-01-31 14:42:08 +0530

    Create nearprime.c

---
+++ nearprime.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int checkprime(int num)
+{
+  int i,flag=0;
+	
+	for(i=2;i<=num/2;i++)
+	{
+		if(num%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	return flag;
+}	
+	
+
+void main()
+{
+  int n,i,flag=0;
+  scanf("%d",&n);
+  if(n>2)
+  {
+   for(i=n;;i++)
+   {
+     flag=checkprime(i);
+     if(flag==1)
+     {
+       printf("The nearest prime number is %d", i);
+       break;
+     }
+    }
+  }
+  else if(n<0)
+  {
+   printf("The Number entered is a negative number");
+  } 
+  else
+  {
+    printf("The nearest prime number is 2");
+  }
+}
+  
+  
+  
