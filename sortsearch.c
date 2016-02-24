12befb68165a8061cd926a644814e952684b094f
VinithaCse <vinitharaocs@gmail.com>
2016-02-24 20:05:08 +0530

    Create sortsearch.c

---
+++ sortsearch.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+  int arr[5],i,val,n,middle,start,end;
+  scanf("%d"&n);
+  for(i=0;i<n;i++)
+  {
+   scanf("%d",&arr[i]);
+  }
+  scanf("%d",&val);
+  
+	while (start<=end) 
+	{
+		middle = (start+end)/2;
+		if (arr[middle] == key)
+		{
+		  printf("%d",middle);
+			    
+		}
+		else if (arr[middle] < key) 
+		{
+			start = middle + 1;
+		}
+		else
+		{
+		  end= middle - 1;
+		}  
+	}
+return 0;
+}
+  
+  
